be2ffb7739f8c51bddb4074fe5c0df931f851e8a
Christian Bandoquillo <bandoquillo.c33@gmail.com>
2020-03-23 01:48:03 +0800

    implemented cards with computed properties

---
+++ resources/js/routes.js
@@ -0,0 +1,24 @@
+import VueRouter from "vue-router";
+
+import Bookables from "./bookables/Bookables.vue";
+import Example2 from "./components/Example2.vue";
+
+const routes = [
+    {
+        path: "/",
+        component: Bookables,
+        name: "home",
+    },
+    {
+        path: "/second",
+        component: Example2,
+        name: "second",
+    }
+];
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
+
+export default router;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>LaravelBNB</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div id ="app"></div>
+        <div id ="app">
+            <index></index>
+        </div>
     </body>
 </html>
